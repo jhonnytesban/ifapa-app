6b2ef19e42e49dfc6739692f7e321c2dfc82640d
Jonathan <jonathanestebangalan@gmail.com>
2022-04-02 18:53:09 +0200

    CASH-0003 Add CASH-0003

--- src/components/Graphic.tsx
+++ src/components/Graphic.tsx
@@ -8,9 +8,15 @@ interface FormDate {
   final: string
 }
 
+interface OptionData {
+  name: string,
+  color: string
+}
+
 const Graphic = () => {
   const { stationData } = useContext(AppContext);
   const [formDate, setFormDate] = useState<FormDate>({begin: '', final: ''});
+  const [formOptions, setFormOptions] = useState<OptionData[]>([])
   const [dataWeatherTimeInterval, setDataWeatherTimeInterval] = useState<WeatherData[]>()
   const [heigthChart, setHeigthChart] = useState<number>()
 
@@ -23,7 +29,7 @@ const Graphic = () => {
     setHeigthChart(250)
   }, [])
   
-
+  
   const handleSubmit = async(event: BaseSyntheticEvent) => {
     event.preventDefault()
     try {
@@ -35,6 +41,28 @@ const Graphic = () => {
     }
   }
 
+  const handleOptionsForm = (event: BaseSyntheticEvent) => {
+    if (event.target.checked) {
+      let simbolos, color;
+	      simbolos = "0123456789ABCDEF";
+	      color = "#";
+
+	    for(var i = 0; i < 6; i++){
+		    color = color + simbolos[Math.floor(Math.random() * 16)];  
+      }
+      const option: OptionData = {
+        name: event.target.name,
+        color: color
+      }
+      setFormOptions([...formOptions, option])
+    }
+
+    if (!event.target.checked) {
+      const Options = formOptions;
+      setFormOptions(Options.filter(option => option.name !== event.target.name))
+    }
+  }
+
   return (
     <>
       {
@@ -42,22 +70,37 @@ const Graphic = () => {
         <form className="container-form-date" onSubmit={handleSubmit}>
           <input type="date" value={formDate.begin} onChange={(event) => setFormDate({...formDate, begin: event.target.value})}/>
           <input type="date" value={formDate.final} onChange={(event) => setFormDate({...formDate, final: event.target.value})}/>
+
+          <label htmlFor="tempMax">Temperatura Máx</label>
+          <input type="checkbox" name="tempMax" id="tempMax" onChange={handleOptionsForm}/>
+
+          <label htmlFor="tempMin">Temperatura Min</label>
+          <input type="checkbox" name="tempMin" id="tempMin" onChange={handleOptionsForm}/>
           <input type="submit" value="Buscar" />
         </form>
       }
       {
-        dataWeatherTimeInterval && 
-        <ResponsiveContainer width='80%' height={heigthChart}>
-          <LineChart data={dataWeatherTimeInterval}
-            margin={{top: 50, right: 5, left: 5, bottom: 5}}>
+        (dataWeatherTimeInterval && formOptions.length > 0 && stationData) &&
+        (
+          <ResponsiveContainer width='80%' height={heigthChart}>
+            <LineChart data={dataWeatherTimeInterval}
+              margin={{top: 50, right: 5, left: 5, bottom: 5}}>
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey='fecha'/>
             <YAxis />
             <Tooltip />
             <Legend />
-            <Line  type='monotone' dataKey='tempMax' stroke="#82ca9d"/>
+            {
+              formOptions.map((option) => {
+                
+                return (
+                  <Line  type='monotone' dataKey={option.name} stroke={option.color}/>
+                )
+              })
+            }
           </LineChart>
         </ResponsiveContainer>
+        )
       }
     </>
   )
